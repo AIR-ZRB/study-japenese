95f50e49c7268eaa22e225ace8ef6620dd0ea56f
青空 <1057605824@qq.com>
2020-08-13 14:24:04 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,98 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <el-container>
+            <el-header>
+                <h1>青空学日语</h1>
+            </el-header>
+            <el-container>
+                <el-aside width="250px">
+                    <el-row class="tac">
+                        <el-col>
+                            <el-menu
+                                default-active="1"
+                                class="el-menu-vertical-demo"
+                                @open="handleOpen"
+                                @close="handleClose"
+                            >
+                                <el-menu-item-group
+                                    v-for="item in menuList"
+                                    :key="item.title"
+                                >
+                                    <template slot="title">{{item.title}}</template>
+                                    <el-menu-item 
+                                      v-for="items in item.children"
+                                      :key="items.name"
+                                      index="1-1"
+                                    >
+                                      {{items.name}}
+                                    </el-menu-item >
+                                </el-menu-item-group>
+                            </el-menu>
+                        </el-col>
+                    </el-row>
+                </el-aside>
+                <el-main>Main</el-main>
+            </el-container>
+        </el-container>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    data() {
+        return {
+            menuList: [
+                {
+                    title: "基础",
+                    children: [{ name: "平假名" }, { name: "固定用法" }],
+                },
+                {
+                    title: "日常用语",
+                    children: [
+                        { name: "介绍" },
+                        { name: "人物" },
+                        { name: "兴趣爱好" },
+                        { name: "饮食" },
+                        { name: "时间" },
+                        { name: "家居" },
+                        { name: "家庭" },
+                        { name: "学校" },
+                        { name: "学校参观" },
+                    ],
+                },
+            ],
+        };
+    },
+    methods: {
+        handleOpen(key, keyPath) {
+            console.log(key, keyPath);
+        },
+        handleClose(key, keyPath) {
+            console.log(key, keyPath);
+        },
+    },
+    created() {},
+};
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    width: 100%;
+    height: 100%;
+}
+// 内容区域
+.el-container {
+    height: 100%;
+}
+// 头部标题栏
+.el-header {
+    border-bottom: 1px solid #ccc;
+    @include flex-layout(space-between);
+}
+// 左侧导航栏
+.el-aside {
+  
+    border-right: 1px solid #ccc;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+// UI框架
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+Vue.use(ElementUI);
+
+// HTTP请求
+import axios from "axios";
+import VueAxios from "vue-axios";
+Vue.use(VueAxios, axios);
+
+// 默认端口
+// axios.defaults.baseURL = "http://localhost:4000/";
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: (h) => h(App),
+}).$mount("#app");
